95e2be24c0b2d90f6b4e2774e51139bebba7630e
PedroVitor37484 <100315461+PedroVitor37484@users.noreply.github.com>
2023-06-28 15:16:21 -0300

    Add files via upload
    Atividade 28.06.2023

---
+++ axercício da aula 2 Pedro Vitor.js
@@ -0,0 +1,23 @@
+var gamer = new Number (300)
+console.log(gamer);
+
+var thunder = new Number(999)
+console.log(thunder);
+
+var vegetable = new Number(21)
+console.log(vegetable);
+
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.EPSILON);
+console.log(Number.MAX_SAFE_INTEGER);
+
+
+var numero4 = new Number(150)
+console.log(numero4)
+
+console.log(numero.toString())
+
+
+// 
